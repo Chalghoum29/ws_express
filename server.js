512ff3f361bdb93f83a933b9cdee4d669eda495c
Chalghoum29 <rekikhedi1997@gmail.com>
2022-01-05 11:34:14 +0100

    1st

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var app = express()
+var PORT = 3000
+
+// app.get('/WorkShop', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.use(date =(req,res,next)=>{
+    var newdate=new Date()
+    var hours=newdate.getHours()
+    var day=newdate.getDay()
+    if((hours<9|| hours>17)||((day==6 || day==0))){
+        res.send('closed')
+    }
+    else{next()}
+   
+    
+})
+
+
+app.use(express.static('Hedi'))
+
+app.listen(3000, (err) => {
+ err? console.error(err) : console.log(`server is running on port ${PORT}`)
+})
